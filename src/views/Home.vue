a1f62639aeb9b29925e134727d02e3e37a531e65
mitra <mitra_khorram@yahoo.com>
2021-05-20 12:36:54 +0430

    fix first page style for login but need add some lable and element

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,137 +1,52 @@
 <template>
   <v-app id="inspire">
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="8">
-            <v-card class="elevation-12">
-              <v-window v-model="step">
+    <v-content >
+      <v-container class="fill-height " fluid >
+        <v-row  justify="center" >
+          <v-col cols="12" sm="6" md="6" >
+            <v-card class="elevation-12 "  justify="center">
+              <v-window v-model="step" >
                 <v-window-item :value="1">
-                  <v-row>
-                    <v-col cols="12" md="8">
+                  <v-row  justify="center">
+                    <v-col cols="12" md="8" >
                       <v-card-text class="mt-12">
                         <h1
                           class="text-center display-2 teal--text text--accent-3"
-                        >Sign in to Diprella</h1>
-                        <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-facebook-f</v-icon>
-                          </v-btn>
-
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-google-plus-g</v-icon>
-                          </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-linkedin-in</v-icon>
-                          </v-btn>
-                        </div>
-                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
-                        <v-form>
+                        >وارد شوید</h1>
+                          <v-form>
                           <v-text-field
-                            label="Email"
-                            name="Email"
+                            label="شماره همراه"
+                            name="phonNumber"
                             prepend-icon="email"
-                            type="text"
+                            type="number"
                             color="teal accent-3"
                           />
 
                           <v-text-field
                             id="password"
-                            label="Password"
+                            label="کد ملی"
                             name="password"
                             prepend-icon="lock"
                             type="password"
                             color="teal accent-3"
                           />
                         </v-form>
-                        <h3 class="text-center mt-4">gfdh?</h3>
-                      </v-card-text>
+                     </v-card-text>
                       <div class="text-center mt-3">
-                        <v-btn rounded color="teal accent-3" dark>SIGN IN</v-btn>
+                        <v-btn rounded color="teal accent-3" dark>ورود</v-btn>
                       </div>
                     </v-col>
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
-                        <h1 class="text-center display-1">Hello, Friend!</h1>
-                        <h5
-                          class="text-center"
-                        >Enter your personal details and start journay with us</h5>
-                      </v-card-text>
-                      <div class="text-center">
-                        <v-btn rounded outlined dark @click="step++">SIGN UP</v-btn>
-                      </div>
-                    </v-col>
+                  
                   </v-row>
                 </v-window-item>
-                <v-window-item :value="2">
-                  <v-row class="fill-height">
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
-                        <h1 class="text-center display-1">Welcome Back!</h1>
-                        <h5
-                          class="text-center"
-                        >To Keep connected with us please login with your personnel info</h5>
-                      </v-card-text>
-                      <div class="text-center">
-                        <v-btn rounded outlined dark @click="step--">Sign in</v-btn>
-                      </div>
-                    </v-col>
-
-                    <v-col cols="12" md="8">
-                      <v-card-text class="mt-12">
-                        <h1 class="text-center display-2 teal--text text--accent-3">Create Account</h1>
-                        <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-facebook-f</v-icon>
-                          </v-btn>
-
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-google-plus-g</v-icon>
-                          </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-linkedin-in</v-icon>
-                          </v-btn>
-                        </div>
-                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
-                        <v-form>
-                          <v-text-field
-                            label="Name"
-                            name="Name"
-                            prepend-icon="person"
-                            type="text"
-                            color="teal accent-3"
-                          />
-                          <v-text-field
-                            label="Email"
-                            name="Email"
-                            prepend-icon="email"
-                            type="text"
-                            color="teal accent-3"
-                          />
-
-                          <v-text-field
-                            id="password"
-                            label="Password"
-                            name="password"
-                            prepend-icon="lock"
-                            type="password"
-                            color="teal accent-3"
-                          />
-                        </v-form>
-                      </v-card-text>
-                      <div class="text-center mt-n5">
-                        <v-btn rounded color="teal accent-3" dark>SIGN UP</v-btn>
-                      </div>
-                    </v-col>
-                  </v-row>
-                </v-window-item>
+              
               </v-window>
             </v-card>
           </v-col>
-        </v-row>
+        </v-row >
       </v-container>
     </v-content>
-  </v-app>
+  </v-app  >
 </template>
 
 <script>
@@ -144,3 +59,8 @@ export default {
   }
 };
 </script>
+<style scoped>
+.found{
+  border:2px solid red;
+}
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,64 @@
+<template>
+      <v-window-item :value="2">
+                  <v-row class="fill-height">
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Welcome Back!</h1>
+                        <h5
+                          class="text-center"
+                        >To Keep connected with us please login with your personnel info</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="step--">Sign in</v-btn>
+                      </div>
+                    </v-col>
+
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1 class="text-center display-2 teal--text text--accent-3">Create Account</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
+                        <v-form>
+                          <v-text-field
+                            label="Name"
+                            name="Name"
+                            prepend-icon="person"
+                            type="text"
+                            color="teal accent-3"
+                          />
+                          <v-text-field
+                            label="Email"
+                            name="Email"
+                            prepend-icon="email"
+                            type="text"
+                            color="teal accent-3"
+                          />
+
+                          <v-text-field
+                            id="password"
+                            label="Password"
+                            name="password"
+                            prepend-icon="lock"
+                            type="password"
+                            color="teal accent-3"
+                          />
+                        </v-form>
+                      </v-card-text>
+                      <div class="text-center mt-n5">
+                        <v-btn rounded color="teal accent-3" dark>SIGN UP</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+</template>
